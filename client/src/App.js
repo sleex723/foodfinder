342aa8bd6954198e6b65e6d4a3a9c76e26d1e446
sleex89 <samlee723@gmail.com>
2018-05-07 18:51:11 -0700

    Added server

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Home from './components/Home/Home';
-// import Results from './components/Results/Results';
+import Results from './components/Results/Results';
 import './App.css';
 
 class App extends Component {
 	render() {
 		return (
-			<Home />
-			// <Results />
+			// <Home />
+			<Results />
 		);
 	}
 }
